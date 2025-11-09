e1cb5d138d8d33db679b84f2e596605746530460
Kedgamer <kedgamer09@gmail.com>
2025-11-09 16:10:29 -0500

    No Gameplay Changes

--- src/main/java/net/kedgamer/ghoc/datagen/ModRecipeProvider.java
+++ src/main/java/net/kedgamer/ghoc/datagen/ModRecipeProvider.java
@@ -1,5 +1,7 @@
 package net.kedgamer.ghoc.datagen;
 
+import com.aizistral.enigmaticlegacy.registries.EnigmaticBlocks;
+import com.aizistral.enigmaticlegacy.registries.EnigmaticItems;
 import net.kedgamer.ghoc.GHoC;
 import net.kedgamer.ghoc.block.ModBlocks;
 import net.kedgamer.ghoc.item.ModItems;
@@ -32,7 +34,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         //oreBlasting(pWriter, GEM_BLOCK_SMELTABLES, RecipeCategory.MISC, ModBlocks.IMPERFECT_GEM_BLOCK.get(), 0.25f, 300, "gem");
 
         //Fuglite from universe core and black concrete powder
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.FUGLITE.get())
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.FUGLITE.get(), 1)
                 .pattern("BBB")
                 .pattern("BUB")
                 .pattern("BBB")
@@ -41,11 +43,79 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .unlockedBy(getHasName(ModItems.UNIVERSE_CORE.get()), has(ModItems.UNIVERSE_CORE.get()))
                 .save(pWriter);
 
-        /*ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.GEM.get(), 9)
-                .requires(ModBlocks.GEM_BLOCK.get())
-                .unlockedBy(getHasName(ModBlocks.GEM_BLOCK.get()), has(ModBlocks.GEM_BLOCK.get()))
-                .save(pWriter);*/
+        /*ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.UNIVERSE_CORE.get(), 1)
+                .pattern("sss")
+                .pattern("ScS")
+                .pattern("eee")
+                .define('s', ModItems.SCROLL_MIXTURE.get())
+                .define('S', ModItems.SPELLSTONE_MIXTURE.get())
+                .define('c', EnigmaticItems.ENIGMATIC_ITEM)
+                .define('e', ModBlocks.ENCHANTED_ETHERIUM.get())
+                .unlockedBy(getHasName(EnigmaticItems.ENIGMATIC_ITEM), has(EnigmaticItems.ENIGMATIC_ITEM))
+                .save(pWriter);
 
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.SKULL_MIXTURE.get(), 1)
+                .pattern(" m ")
+                .pattern(" g ")
+                .pattern(" r ")
+                .define('m', com.Polarice3.Goety.common.blocks.ModBlocks.REDSTONE_MONSTROSITY_HEAD_BLOCK.get())
+                .define('g', com.Polarice3.Goety.common.blocks.ModBlocks.GRAVE_GOLEM_SKULL_BLOCK.get())
+                .define('r', com.Polarice3.Goety.common.blocks.ModBlocks.REDSTONE_GOLEM_SKULL_BLOCK.get())
+                .save(pWriter);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ENCHANTED_ETHERIUM.get(), 1)
+                .pattern("eee")
+                .pattern("ere")
+                .pattern("eee")
+                .define('r', com.Polarice3.Goety.common.blocks.ModBlocks.REDSTONE_MONSTROSITY_HEAD_BLOCK.get())
+                .define('e', EnigmaticBlocks.ETHERIUM_BLOCK)
+                .save(pWriter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SCROLL_MIXTURE.get(), 1)
+                .requires(EnigmaticItems.XP_SCROLL)
+                .requires(EnigmaticItems.ESCAPE_SCROLL)
+                .requires(EnigmaticItems.HEAVEN_SCROLL)
+                .requires(EnigmaticItems.THICC_SCROLL)
+                .requires(EnigmaticItems.FABULOUS_SCROLL)
+                .requires(EnigmaticItems.CURSED_SCROLL)
+                .requires(EnigmaticItems.AVARICE_SCROLL)
+                .requires(EnigmaticItems.DARKEST_SCROLL)
+                .save(pWriter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.RING_MIXTURE.get(), 1)
+                .requires(EnigmaticItems.MAGNET_RING)
+                .requires(EnigmaticItems.SUPER_MAGNET_RING)
+                .requires(EnigmaticItems.ENDER_RING)
+                .requires(EnigmaticItems.IRON_RING)
+                .requires(EnigmaticItems.GOLDEN_RING)
+                .requires(EnigmaticItems.DESOLATION_RING)
+                .save(pWriter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.CHARM_MIXTURE.get(), 1)
+                .requires(EnigmaticItems.ASCENSION_AMULET)
+                .requires(EnigmaticItems.MEGA_SPONGE)
+                .requires(EnigmaticItems.MINING_CHARM)
+                .requires(EnigmaticItems.MONSTER_CHARM)
+                .requires(EnigmaticItems.BERSERK_CHARM)
+                .requires(EnigmaticItems.ENCHANTER_PEARL)
+                .requires(EnigmaticItems.INSIGNIA)
+                .requires(EnigmaticItems.ENIGMATIC_EYE)
+                .requires(EnigmaticItems.ELDRITCH_AMULET)
+                .save(pWriter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SPELLSTONE_MIXTURE.get(), 1)
+                .requires(EnigmaticItems.EYE_OF_NEBULA)
+                .requires(EnigmaticItems.BLAZING_CORE)
+                .requires(EnigmaticItems.VOID_PEARL)
+                .requires(EnigmaticItems.OCEAN_STONE)
+                .requires(EnigmaticItems.ANGEL_BLESSING)
+                .requires(EnigmaticItems.GOLEM_HEART)
+                .requires(EnigmaticItems.THE_CUBE)
+                .requires(EnigmaticItems.COSMIC_HEART)
+                .requires(EnigmaticItems.COSMIC_HEART)
+                .save(pWriter);
+
+         */
 
     }
 
